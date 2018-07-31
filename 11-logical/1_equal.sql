87c2553aee12a496ae9aa68f09ae9ae548e0dd3c
bokoness <bokoness@gmail.com>
2018-07-31 11:12:14 +0000

    insert data into customers and orders table

--- 11-logical/1_equal.sql
+++ 11-logical/1_equal.sql
@@ -1,5 +1,8 @@
 /************
 | NOT EQUAL |
 ************/
+
+-- select all the books that wrote in 2017
+SELECT title, released_year AS '2017 books' FROM books WHERE released_year=2003;
 -- select all the books exept the ones that wrote in 2017
-SELECT title FROM books WHERE year != 2017;
+SELECT title, released_year AS 'All books exept 2017' FROM books WHERE released_year!=2017;
